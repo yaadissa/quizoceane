10442e3319088f261df7641a03e597b22ffa37d3
Youness AADISSA <yaadissa@oceane.local>
2017-06-06 12:53:47 +0200

    init project

---
+++ application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php 
+		$sq_base_url='http://quizoceane.azurewebsites.net/';
+		$sq_hostname='oceane-test-auto.database.windows.net';
+		$sq_dbname='quizz_db';
+		$sq_dbusername='admin_oceane';
+		$sq_dbpassword='EnergieJoule01';
+		?>
---
+++ application/views/quiz_list.php
@@ -0,0 +1,113 @@
+ <div class="container">
+<?php 
+$logged_in=$this->session->userdata('logged_in');
+			 
+			
+			?>
+   
+ <h3><?php echo $title;?></h3>
+    <?php 
+	if($logged_in['su']=='1'){
+		?>
+		<div class="row">
+ 
+  <div class="col-lg-6">
+    <form method="post" action="<?php echo site_url('quiz/index/');?>">
+	<div class="input-group">
+    <input type="text" class="form-control" name="search" placeholder="<?php echo $this->lang->line('search');?>...">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="submit"><?php echo $this->lang->line('search');?></button>
+      </span>
+	 
+	  
+    </div><!-- /input-group -->
+	 </form>
+  </div><!-- /.col-lg-6 -->
+</div><!-- /.row -->
+
+<?php 
+	}
+?>
+
+  <div class="row">
+ 
+<div class="col-md-12">
+<br> 
+			<?php 
+		if($this->session->flashdata('message')){
+			echo $this->session->flashdata('message');	
+		}
+		?>	
+		
+<table class="table table-bordered">
+<tr class="info">
+ <th>#</th>
+ <th><?php echo $this->lang->line('quiz_name');?></th>
+<th><?php echo $this->lang->line('noq');?></th>
+<th><?php echo $this->lang->line('start_date_s');?></th>
+<th><?php echo $this->lang->line('end_date_s');?></th>
+<th><?php echo $this->lang->line('action');?> </th>
+</tr>
+<?php 
+if(count($result)==0){
+	?>
+<tr>
+ <td colspan="3"><?php echo $this->lang->line('no_record_found');?></td>
+</tr>	
+	
+	
+	<?php
+}
+foreach($result as $key => $val){
+		$classy='';
+	if(( $val['start_date'] < time() &&  time() < $val['end_date'] ) || $logged_in['su']==1){
+		if((time() < $val['start_date']  || time() > $val['end_date'] )){
+		$classy='danger';
+	}
+?>
+<tr class="<?php echo $classy ?>">
+ <td><?php echo $val['quid'];?></td>
+ <td><?php echo substr(strip_tags($val['quiz_name']),0,50);?></td>
+<td><?php echo $val['noq'];?></td>
+<td><?php echo date('d-m-Y H:i:s',$val['start_date']);?></td>
+<td><?php echo date('d-m-Y H:i:s',$val['end_date']);?></td>
+ <td>
+<a href="<?php echo site_url('quiz/quiz_detail/'.$val['quid']);?>" class="btn btn-success"  ><?php echo $this->lang->line('attempt');?> </a>
+
+<?php 
+if($logged_in['su']=='1'){
+	?>
+			
+<a href="<?php echo site_url('quiz/edit_quiz/'.$val['quid']);?>"><img src="<?php echo base_url('images/edit.png');?>"></a>
+<a href="javascript:remove_entry('quiz/remove_quiz/<?php echo $val['quid'];?>');"><img src="<?php echo base_url('images/cross.png');?>"></a>
+<?php 
+}
+?>
+</td>
+</tr>
+
+<?php 
+}
+}
+?>
+</table>
+</div>
+
+</div>
+
+
+<?php
+if(($limit-($this->config->item('number_of_rows')))>=0){ $back=$limit-($this->config->item('number_of_rows')); }else{ $back='0'; } ?>
+
+<a href="<?php echo site_url('quiz/index/'.$back);?>"  class="btn btn-primary"><?php echo $this->lang->line('back');?></a>
+&nbsp;&nbsp;
+<?php
+ $next=$limit+($this->config->item('number_of_rows'));  ?>
+
+<a href="<?php echo site_url('quiz/index/'.$next);?>"  class="btn btn-primary"><?php echo $this->lang->line('next');?></a>
+
+
+
+
+
+</div>
